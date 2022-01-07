8ab100f9b2f724e4d179d7441871cc5fc853ff2c
Mariana <mariana.rendon@unosquare.com>
2022-01-07 17:28:19 -0600

    assert not containstext

--- Tests/unosquare.js
+++ Tests/unosquare.js
@@ -6,7 +6,7 @@
       .waitForElementVisible('body')
       .end();
     }
-  };*/
+  };
 
   module.exports = {
     'Demo test unosquare' : function(browser) {
@@ -20,4 +20,18 @@
       .url('https://www.amazon.com')
       .end();
     }
-  };
+  };*/
+
+  module.exports = {
+    'Demo test unosquare' : function(browser) {
+      browser
+      .windowMaximize()
+      .url('https://www.unosquare.com')
+      .waitForElementVisible('body')
+      .assert.attributeContains("li a[href = '/Services']", 'class', 'nav')
+      .assert.attributeEquals("li a[href = '/Services']", 'class', 'nav-link')
+      .waitForElementVisible("li a[href = '/Services']")
+      .assert.containsText("li a[href = '/Services']", "SERVICES"); //This is case Sensitive
+      browser.end();
+    }
+  }
