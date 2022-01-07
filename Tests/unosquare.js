a050dd00080c46605639108b59961e4492bf39db
Mariana <mariana.rendon@unosquare.com>
2022-01-07 17:20:01 -0600

    assert not attribute equals

--- Tests/unosquare.js
+++ Tests/unosquare.js
@@ -6,7 +6,7 @@
       .waitForElementVisible('body')
       .end();
     }
-  };*/
+  };
 
   module.exports = {
     'Demo test unosquare' : function(browser) {
@@ -18,6 +18,18 @@
       .url('https://www.google.com')
       .assert.visible('.non_existing')
       .url('https://www.amazon.com')
+      .end();
+    }
+  };*/
+
+  module.exports = {
+    'Demo test unosquare' : function(browser) {
+      browser
+      .windowMaximize()
+      .url('https://www.unosquare.com')
+      .waitForElementVisible('body')
+      .assert.attributeContains("li a[href = '/Services']", 'class', 'nav')
+      .assert.attributeEquals("li a[href = '/Services']", 'class', 'nav-link')
       .end();
     }
   };
